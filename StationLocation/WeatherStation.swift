5b130466ca05810d4f30562ba783aef97835fb0c
Justin Andrews <justinandrews@Justins-MacBook-Air.local>
2017-11-16 21:26:41 -0800

    Created a WundergroundQueryResult struct to decode a Wunderground JSON response based on a template response obtained from the API section of their website.  Running the actual query and fetching the data will be added later. Updated the WeatherStation class to support the Decodable protocol, which meant removing the CLLocationCoordinate coordinate property in favor of a getter that converts the raw string data.

--- StationLocation/WeatherStation.swift
+++ StationLocation/WeatherStation.swift
@@ -9,10 +9,43 @@
 import Foundation
 import MapKit
 
-class WeatherStation: NSObject , MKAnnotation {
-
-    let coordinate: CLLocationCoordinate2D
-    init(_ location:CLLocationCoordinate2D){
-        coordinate = location
+class WeatherStation: NSObject , Decodable {
+    
+    /*
+        Some properties are optional because the data is dependent on station type (airport/pws) but
+     there is enough overlap I'm using the same class for both.
+     */
+    
+    // Prevent access to raw string data, prefer .coordinate
+    private var longitude: String? = nil
+    private var latitude: String? = nil
+    
+    var neighborhood: String? = nil
+    var city: String = ""
+    var state: String = ""
+    var country: String = ""
+    
+    private enum CodingKeys : String , CodingKey {
+        case longitude = "lon"
+        case latitude = "lat"
+        case neighborhood
+        case city
+        case state
+        case country
     }
 }
+
+extension WeatherStation: MKAnnotation
+{
+    var coordinate: CLLocationCoordinate2D {
+        get{
+            var coord = kCLLocationCoordinate2DInvalid
+            if let lat = latitude , let lon = longitude {
+                coord = CLLocationCoordinate2D.init(latitude: Double(lat) ?? 0 ,
+                                                    longitude: Double(lon) ?? 0 )
+            }
+            return coord
+        }
+    }
+}
+
---
+++ StationLocation/WundergroundQueryResult.swift
@@ -0,0 +1,72 @@
+//
+//  WundergroundQueryResult.swift
+//  StationLocation
+//
+//  Created by Justin Andrews on 11/16/17.
+//  Copyright © 2017 Justin Andrews. All rights reserved.
+//
+
+import Foundation
+
+struct WundergroundQueryResult : Decodable // Encoding not needed now.
+{
+    let location: Location
+    let observation: WeatherObservation
+    
+    private enum CodingKeys: String , CodingKey {
+        case observation = "current_observation"
+        case location
+    }
+    
+    // convenience method for access to all WeatherStation(s) for query
+    var stations : [WeatherStation]? {
+        get{
+            var stations : [WeatherStation]?
+            if let airportStations = location.nearbyStations?["airport"]?["station"]{
+                stations = airportStations
+            }
+            if let pwsStations = location.nearbyStations?["pws"]?["station"]{
+                stations?.append(contentsOf: pwsStations)
+            }
+            return stations
+        }
+    }
+}
+
+struct Location : Decodable
+{
+    var city: String?
+    var country: String?
+    var latitude: String?
+    var longitude: String?
+
+    /*
+     Instead of a custom decoding setup, rely on automatic decoding and custom
+     getters to parse data needed for the interface.  See WundergroundQueryResult.stations
+     */
+    fileprivate var nearbyStations: [String: [String: [WeatherStation] ] ]?
+
+    private enum CodingKeys : String , CodingKey
+    {
+        case city
+        case country
+        case latitude = "lat"
+        case longitude = "lon"
+        case nearbyStations = "nearby_weather_stations"
+    }
+
+}
+
+struct WeatherObservation : Decodable
+{
+    let temperature: String?
+    let temperatureF: Double?
+    let temperatureC: Double?
+    
+    private enum CodingKeys: String , CodingKey {
+        case temperature = "temperature_string"
+        case temperatureF = "temp_f"
+        case temperatureC = "temp_c"
+    }
+    
+}
